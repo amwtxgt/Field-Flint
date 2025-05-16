27e8e749fe3b180f241f3ba543f6f86bd416d908
amwtxgt <88107477@qq.com>
2025-05-17 00:43:14 +0800

    Update Electron and enable second window functionality
    Bump Electron to version 36.2.1 and uncomment code to enable loading and displaying the second window (Window2) alongside the main window, including both development and production environments. A minor unrelated change was made in Window2's template.

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,38 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import fs from 'node:fs'
 import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify'
 import electronPlugin from 'vite-plugin-electron'
 import {resolve} from 'path'
+import {ChildProcessWithoutNullStreams, spawn} from 'child_process'
+import electronPath from 'electron'
 
 fs.rmSync('dist', {recursive: true, force: true})
+//@ts-ignore
+console.log('111111111黄敏',process.electronApp)
+let electronApp: ChildProcessWithoutNullStreams | undefined
+
+//重新加载应用
+function reloadElectronApp() {
+	if (electronApp) {
+		electronApp.removeAllListeners()
+		electronApp.kill()
+	}
+
+	// Start Electron.app
+	electronApp = spawn(electronPath as unknown as string, ['.', '--no-sandbox'])
+	// Exit command after Electron.app exits
+	electronApp.once('exit', process.exit)
+
+	electronApp.stdout?.on('data', (data) => {
+		const str = data.toString().trim()
+		str && console.log(str)
+	})
+	electronApp.stderr?.on('data', (data) => {
+		const str = data.toString().trim()
+		str && console.error(str)
+	})
+}
 
 
 export default defineConfig({
@@ -17,10 +44,8 @@ export default defineConfig({
 		electronPlugin([
 			{
 				entry: 'electron/main.ts',
-				onstart({startup}) {
-					startup().then(async () => {
-
-					})
+				onstart() {
+					reloadElectronApp()
 				},
 				vite: {
 					build: {
@@ -35,7 +60,7 @@ export default defineConfig({
 			{
 				entry: 'preloads/index.ts',
 				onstart(options) {
-					options.reload()
+					reloadElectronApp()
 				},
 				vite: {
 					build: {
@@ -62,7 +87,7 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			'@': resolve(__dirname,'src'),
+			'@': resolve(__dirname, 'src'),
 		},
 	},
 
